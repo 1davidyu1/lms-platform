30bd0005dfef41a7fa7834e5c59989645a74c9a4
1davidyu1 <yudavid@msu.edu>
2024-01-11 18:02:57 -0500

    course title form p1

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,11 @@
-import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
-import { IconBadge } from '@/components/icon-badge'
 import { LayoutDashboard } from 'lucide-react'
+
+import { db } from '@/lib/db'
+import { IconBadge } from '@/components/icon-badge'
+
+import { TitleForm } from './_components/title-form'
 
 const CourseIdPage = async ({
     params
@@ -57,6 +60,11 @@ const CourseIdPage = async ({
                                 Customize your course
                             </h2>
                         </div>
+                        <TitleForm
+                            initialData={course}
+                            courseId={course.id}
+
+                         />
                     </div>
                 </div>
         </div>
